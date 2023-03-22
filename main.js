915a54b5f8672e3215d9782512a3a0d52e88b26e
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-22 11:58:09 -0600

    initial commit. basic working project

---
+++ main.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(5150, () => {
+    console.log("app is up on 5150")
+})
