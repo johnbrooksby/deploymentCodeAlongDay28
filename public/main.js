a34040e3926d8faaf216f8c75232fe29b56c83ca
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-22 12:26:38 -0600

    Now serves front-end files from back-end end points

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat\'s name is: Rascal')
 
 btn.addEventListener('click', clickHandler)
